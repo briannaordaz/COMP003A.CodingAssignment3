fee7fdd0a4ec2252eb49d60f095f8bf932d479e3
briannaordaz <briannaordaz5@gmail.com>
2025-01-29 18:14:36 -0800

    added author, course, faculty, and purpose

---
+++ COMP003A.CodingAssignment3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Author: Brianna Ordaz
+//Course: COMP003A
+//Faculty: Jonathan Cruz
+//Purpose: Budget management application demonstrating control flow.
+namespace COMP003A.CodingAssignment3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
