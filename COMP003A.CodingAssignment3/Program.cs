1fca932172fdd2b361b3be740537283140d7a22a
briannaordaz <briannaordaz5@gmail.com>
2025-01-29 20:13:30 -0800

    added a menu

--- COMP003A.CodingAssignment3/Program.cs
+++ COMP003A.CodingAssignment3/Program.cs
@@ -4,10 +4,21 @@
 //Purpose: Budget management application demonstrating control flow.
 namespace COMP003A.CodingAssignment3;
 
-class Program
+internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter your monthly income: ");
+        int income = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("1. Add an Expense");
+        Console.WriteLine("2. View Expenses and Budget");
+        Console.WriteLine("3. Remove an Expense");
+        Console.WriteLine("4. Exit");
+        
+        
+
+
+
     }
 }
--- COMP003A.CodingAssignment3/obj/Debug/net8.0/COMP003A.CodingAssignment3.AssemblyInfo.cs
+++ COMP003A.CodingAssignment3/obj/Debug/net8.0/COMP003A.CodingAssignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP003A.CodingAssignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee7fdd0a4ec2252eb49d60f095f8bf932d479e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP003A.CodingAssignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP003A.CodingAssignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
